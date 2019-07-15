07741c5b23ab56dcc353ee43349cf8a85988f59e
programer10 <programer10@ucione.local>
2019-07-15 21:12:43 +0200

    Added Users CRUD

--- routes/web.php
+++ routes/web.php
@@ -20,10 +20,29 @@ Route::get('/posts', 'PostController@index')->name('posts.index');
 
 Route::get('/posts/{id}', 'PostController@show')->name('posts.show');
 
+
 /***************** USERS *******************/
-Route::get('/users', function () {
-    return view('users.index');
-});
+// prikaži sve usere
+Route::get('/users', 'UserController@index')->name('users.index');
+
+// prikaži formu za kreiranje usera
+Route::get('/users/create', 'UserController@create')->name('users.create');
+
+// spremi usera u bazu
+Route::post('/users', 'UserController@store')->name('users.store');
+
+// prikaži pojedinog usera
+Route::get('/users/{user}', 'UserController@show')->name('users.show');
+
+// prikaži formu za uređivanje usera
+Route::get('/users/{user}/edit', 'UserController@edit')->name('users.edit');
+
+// obriši usera
+Route::delete('/users/{user}', 'UserController@destroy')->name('users.destroy');
+
+
+
+
 
 Auth::routes();
 
